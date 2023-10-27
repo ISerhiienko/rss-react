16c8620251cd879fe5eac3b4274bc7f1abdd2be5
Ihor Serhiienko <igorserhiyenko@gmail.com>
2023-10-27 19:23:55 +0300

    feat: add logic to App component

--- rss-react/src/App.tsx
+++ rss-react/src/App.tsx
@@ -1,27 +1,91 @@
-import { useState } from "react";
+import { Component } from 'react';
 
-import "./App.css";
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+import SearchBar from './components/SearchBar';
+import SearchResult from './components/SearchResult';
 
-  return (
-    <>
-      <div></div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+interface AppState {
+  searchResults: {
+    name: string;
+    height: string;
+    mass: string;
+    hair_color: string;
+    skin_color: string;
+    eye_color: string;
+    birth_year: string;
+    gender: string;
+    homeworld: string;
+    films: string[];
+    species: string[];
+    vehicles: string[];
+    starships: string[];
+    created: string;
+    edited: string;
+    url: string;
+  }[];
+  error: string | null;
+  loading: boolean;
+}
+
+class App extends Component<object, AppState> {
+  constructor(props: object) {
+    super(props);
+    this.state = {
+      searchResults: [],
+      error: null,
+      loading: false,
+    };
+  }
+
+  handleSearch = (searchTerm: string) => {
+    if (searchTerm.trim() === '') {
+      this.fetchSearchResults('');
+    } else {
+      this.fetchSearchResults(searchTerm.trim());
+    }
+  };
+
+  fetchSearchResults = (searchTerm: string) => {
+    this.setState({ loading: true });
+    fetch(`https://swapi.dev/api/people/?search=${searchTerm}&page=1`)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.results) {
+          this.setState({
+            searchResults: data.results,
+            error: null,
+            loading: false,
+          });
+        } else {
+          this.setState({
+            searchResults: [],
+            error: 'No results found',
+            loading: false,
+          });
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+        this.setState({
+          searchResults: [],
+          error: error.message,
+          loading: false,
+        });
+      });
+  };
+
+  render() {
+    const { searchResults, error, loading } = this.state;
+
+    return (
+      <div className="container">
+        <h1>Search StarWars People</h1>
+        <SearchBar onSearch={this.handleSearch} />
+        <SearchResult results={searchResults} error={error} loading={loading} />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+    );
+  }
 }
 
 export default App;
