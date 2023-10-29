f61dc96bd060116b9469e3c5244b869034ce194a
Ihor Serhiienko <igorserhiyenko@gmail.com>
2023-10-29 17:02:07 +0200

    refactor: convert SearchResult FC to class component

--- rss-react/src/components/SearchResult.tsx
+++ rss-react/src/components/SearchResult.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 interface SearchResultProps {
   results: {
@@ -23,36 +23,35 @@ interface SearchResultProps {
   loading: boolean;
 }
 
-const SearchResult: React.FC<SearchResultProps> = ({
-  results,
-  error,
-  loading,
-}) => {
-  return (
-    <div className="bottom-section">
-      {loading ? (
-        <p>Loading data ...</p>
-      ) : error ? (
-        <p>{error}</p>
-      ) : results.length === 0 ? (
-        <p>Nothing found</p>
-      ) : (
-        results &&
-        results.map((result, index) => (
-          <div key={index} className="item">
-            <h2>{result.name}</h2>
-            <p>Height: {result.height}</p>
-            <p>Mass: {result.mass}</p>
-            <p>Hair Color: {result.hair_color}</p>
-            <p>Skin Color: {result.skin_color}</p>
-            <p>Eye Color: {result.eye_color}</p>
-            <p>Birth Year: {result.birth_year}</p>
-            <p>Gender: {result.gender}</p>
-          </div>
-        ))
-      )}
-    </div>
-  );
-};
+class SearchResult extends Component<SearchResultProps> {
+  render() {
+    const { results, error, loading } = this.props;
+    return (
+      <div className="bottom-section">
+        {loading ? (
+          <p>Loading data ...</p>
+        ) : error ? (
+          <p>{error}</p>
+        ) : results.length === 0 ? (
+          <p>Nothing found</p>
+        ) : (
+          results &&
+          results.map((result, index) => (
+            <div key={index} className="item">
+              <h2>{result.name}</h2>
+              <p>Height: {result.height}</p>
+              <p>Mass: {result.mass}</p>
+              <p>Hair Color: {result.hair_color}</p>
+              <p>Skin Color: {result.skin_color}</p>
+              <p>Eye Color: {result.eye_color}</p>
+              <p>Birth Year: {result.birth_year}</p>
+              <p>Gender: {result.gender}</p>
+            </div>
+          ))
+        )}
+      </div>
+    );
+  }
+}
 
 export default SearchResult;
